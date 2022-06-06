fa2b7fc3f7414bf2bcc51a5b89863cc19531d6af
Ty <tycloud97@gmail.com>
2022-06-07 00:38:06 +0700

    move state

--- main.tf
+++ main.tf
@@ -10,9 +10,16 @@ terraform {
       version = "~> 3.27"
     }
   }
-
+    backend "s3" {
+        bucket = "827539266883-terraform-state"
+        encrypt = true
+        key = "terraform.tfstate"
+        region = "ap-southeast-1"
+    }
   required_version = ">= 0.14.9"
 }
+
+
 
 data "aws_caller_identity" "current" {}
 
