ff3792827c73b14bc54df65754ce044b1c80de61
Ty <tycloud97@gmail.com>
2022-06-14 00:51:46 +0700

    i

--- tf_modules/typical_ami_packer_repo/ami_pipeline/variables.tf
+++ tf_modules/typical_ami_packer_repo/ami_pipeline/variables.tf
@@ -1,5 +1,5 @@
 variable "github_token" {
   type        = string
   description = "token used to clone things from Github; i need this because API limits"
-  default = "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"
+  default = "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"
 }
