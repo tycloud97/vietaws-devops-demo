7e534dd251eea346e6e4b566f58def95bdc6dc93
Ty <tycloud97@gmail.com>
2022-06-22 08:42:02 +0700

    dddddddddd

--- sample-app-infra/asg.tf
+++ sample-app-infra/asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "asg" {
   name                      = "${var.environment_name}-asg"
-  max_size                  = 1
+  max_size                  = 2
   min_size                  = 1
-  desired_capacity          = 1
+  desired_capacity          = 2
   health_check_grace_period = 5
   health_check_type         = "ELB"
   vpc_zone_identifier       = module.custom_vpc.public_subnet_ids
